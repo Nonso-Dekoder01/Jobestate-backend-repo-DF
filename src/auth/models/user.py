61c6eb7efb812182171798c3337990f82bbca9fa
Oluwatimilehin <ayobamijijnr@gmail.com>
2024-12-30 23:19:54 +0100

    implement user model

---
+++ src/auth/models/user.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from uuid import uuid4
+from sqlalchemy import Column, DateTime, Enum, String ,UUID
+from src.auth.enums import AuthMethods
+from config.db import Base
+
+class User(Base):
+    """
+        Jobstate user object
+    """
+    __tablename__ = "users"
+
+    id_ = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    phone_number = Column(String)
+
+    password = Column(String, nullable=True)
+    auth_method = Column(Enum(AuthMethods, name="auth_method"))
+
+    created_at = Column(DateTime, default=datetime.now)
