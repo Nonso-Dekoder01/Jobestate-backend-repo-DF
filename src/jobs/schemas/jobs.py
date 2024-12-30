c0e0bd9061fa22dfffd91f7b2d4d175ee8f1b557
Oluwatimilehin <ayobamijijnr@gmail.com>
2024-12-31 00:19:37 +0100

    implement job create schema

---
+++ src/jobs/schemas/jobs.py
@@ -0,0 +1,19 @@
+from typing import List,Optional
+from pydantic import BaseModel
+
+from src.jobs.enums import JobTypes, SalaryPeriod
+
+class JobCreate(BaseModel):
+	title: str
+	description: str
+	minimum_salary: float
+	maximum_salary: float
+	company_name: str
+	job_type: JobTypes
+	salary_period: SalaryPeriod
+	job_category_ids: List[int]
+
+
+
+class JobsUpdate(BaseModel):
+	pass
