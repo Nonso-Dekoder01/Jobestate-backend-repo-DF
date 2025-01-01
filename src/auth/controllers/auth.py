01c8aac215fe45dac2421604238313b4ef4e32ee
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-01 11:35:41 +0100

    new login and register methods

--- src/auth/controllers/auth.py
+++ src/auth/controllers/auth.py
@@ -1,6 +1,12 @@
 from fastapi import Depends
+
 from config.db import get_db
+
 from src.auth.services import AuthService
+from src.auth.schemas import (
+    LoginSchema, 
+    RegisterSchema
+)
 
 class AuthController:
     @staticmethod
@@ -23,4 +29,30 @@ class AuthController:
         try:
             return await AuthService.verify_user(code,db)
         except Exception as exc:
+            return str(exc)
+        
+    
+    @staticmethod
+    async def signup(
+        payload: RegisterSchema,
+        db = Depends(get_db)
+    ):
+        """
+            Sign up with email and password
+        """
+        try:
+            return await AuthService.signup(payload, db)
+        except Exception as exc:
+            return str(exc)
+        
+    
+    @staticmethod
+    async def login(
+        payload: LoginSchema,
+        db = Depends(get_db)
+    ):
+        try:
+            return await AuthService.login(payload, db)
+        except Exception as exc:
+            print(exc)
             return str(exc)
