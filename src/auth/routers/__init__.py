54747e6f4e5f8ae7c9e32ba25078475122633518
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-01 11:36:00 +0100

    add new routes

--- src/auth/routers/__init__.py
+++ src/auth/routers/__init__.py
@@ -8,5 +8,11 @@ auth_router = APIRouter(prefix="/auth")
 auth_router.get("")\
     (AuthController.google_auth)
 
+auth_router.post("/signup")\
+    (AuthController.signup)
+
+auth_router.post("/login")\
+    (AuthController.login)
+
 auth_router.get("/google")\
     (AuthController.verify_user)
