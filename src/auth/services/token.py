bee96145a03c456aaa3d88f9c01f0dbc5e4f4f48
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-01 11:37:05 +0100

    implement token service

---
+++ src/auth/services/token.py
@@ -0,0 +1,30 @@
+from datetime import datetime,timedelta
+import os
+from fastapi import HTTPException
+import jwt
+from src.auth.schemas import DataInToken
+
+class TokenService:
+    @staticmethod
+    async def create_access_token(data: DataInToken, expires_delta: timedelta = timedelta(minutes=2880)):
+        try:
+            SECRET_KEY = os.getenv('SECRET_KEY')
+
+            expires = datetime.now() + expires_delta
+            data.user.id_ = str(data.user.id_)
+            data.exp = expires
+            return jwt.encode(data.model_dump(), SECRET_KEY, algorithm="HS256")
+        except Exception as exc:
+            raise exc
+        
+    
+    @staticmethod
+    def decode_token(token: str):
+        try:
+            SECRET_KEY = os.getenv('SECRET_KEY')
+            payload = jwt.decode(token.split(" ")[-1], SECRET_KEY, algorithms=["HS256"])
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(401, "Token has expired")
+        except jwt.InvalidTokenError:
+            raise HTTPException(401, "Invalid token")
