dc9d4aaf8976ff2383ab6b4763b41ee3d36171db
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-02 13:28:28 +0100

    new  columns for user model

--- src/auth/models/user.py
+++ src/auth/models/user.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import uuid4
-from sqlalchemy import Column, DateTime, Enum, String ,UUID
-from src.auth.enums import AuthMethods, Roles
+from sqlalchemy import Boolean, Column, DateTime, Enum, String ,UUID
+from src.auth.enums import AuthMethods, Roles, Status as UserStatus
 from config.db import Base
 
 class User(Base):
@@ -20,4 +20,7 @@ class User(Base):
     password = Column(String, nullable=True)
     auth_method = Column(Enum(AuthMethods, name="auth_method"))
 
+    is_phone_verified = Column(Boolean, default=False)
+    is_email_verified = Column(Boolean, default=False)
+    status = Column(Enum(UserStatus, name="user_status"), default=UserStatus.PENDING_VERIFICATION)
     created_at = Column(DateTime, default=datetime.now)
