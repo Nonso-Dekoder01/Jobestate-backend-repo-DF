8b73b8f58ba54c47e1385d1e2d635e21f7f84755
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-02 12:42:52 +0100

    add request validation error handler

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter,FastAPI
+from fastapi import APIRouter,FastAPI, Request
+from fastapi.exceptions import RequestValidationError
 
 from config.db import Base, engine
+from errors import validation_exception_handler
 from src.auth.routers import auth_router
 from src.jobs.routers import jobs_router
 
@@ -9,10 +11,19 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app_router = APIRouter(prefix="/api")
+
 app_router.include_router(auth_router)
 app_router.include_router(jobs_router)
 
+
 app.include_router(app_router)
+
+# To handle parsing errors from request bodies
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler_main(request:Request, exc: RequestValidationError):
+    return validation_exception_handler(exc)
+
+
 
 # To start, you can use
 # python main.py or
