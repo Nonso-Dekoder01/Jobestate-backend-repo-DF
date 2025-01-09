0ae6289a1833f01477578ed018421881b38ab800
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-09 15:51:11 +0100

    create new method

--- src/jobs/services/job_categories.py
+++ src/jobs/services/job_categories.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from errors import raise_error
 from src.jobs.models import JobCategory
+from src.jobs.schemas import JobCategoryCreate
 
 
 class JobCategoryService:
@@ -49,5 +50,43 @@ class JobCategoryService:
                 job_categories.append(await JobCategoryService.find_by_id(job_category_id, db))
             
             return job_categories
+        except Exception as exc:
+            raise_error(exc)
+
+
+    @staticmethod
+    async def find_by_name(
+        name: str,
+        db: Session
+    ):
+        try:
+            job = db.query(JobCategory).filter_by(name=name.capitalize().strip()).first()
+
+            if not job:
+                raise HTTPException(
+                    404,
+                    f"Job {name} not found"
+                )
+
+            return job
+        except Exception as exc:
+            raise_error(exc)
+    
+    
+    @staticmethod
+    async def create(
+        payload: JobCategoryCreate,
+        db: Session
+    ):
+        try:
+            await JobCategoryService.find_by_name(payload.name)
+            job = JobCategory(
+                name=payload.name.capitalize().strip()
+            )
+
+            db.add(job)
+            db.commit()
+            db.refresh(job)
+            return job
         except Exception as exc:
             raise_error(exc)
