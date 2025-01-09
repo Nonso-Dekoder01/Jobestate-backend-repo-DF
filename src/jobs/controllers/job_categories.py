92c083c070337605ff431d3f476d9763d447c938
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-09 15:59:49 +0100

    add new controller

---
+++ src/jobs/controllers/job_categories.py
@@ -0,0 +1,36 @@
+from fastapi import Depends
+
+from config.db import get_db
+from errors import parse_error
+from response import Response
+from src.jobs.services import JobCategoryService
+from src.jobs.schemas import JobCategoryCreate
+
+
+class JobCategoriesController:
+    @staticmethod
+    async def create(
+        payload: JobCategoryCreate,
+        db = Depends(get_db)
+    ):
+        try:
+            job_category = await JobCategoryService.create(payload,db)
+
+            return Response(job_category)
+        except Exception as exc:
+            return parse_error(exc)
+        
+
+    async def get(
+        db = Depends(get_db)
+    ):
+        try:
+            job_categories = await JobCategoryService.get_all(db)
+
+            return Response(job_categories)
+        except Exception as exc:
+            return parse_error(exc)
+    
+
+    
+    
