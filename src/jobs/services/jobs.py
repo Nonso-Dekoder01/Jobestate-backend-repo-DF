7a233e154a62744232e426eca7ab9f68fd8d9809
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-01-10 14:44:58 +0100

    implement update service

--- src/jobs/services/jobs.py
+++ src/jobs/services/jobs.py
@@ -1,9 +1,16 @@
+"""
+
+"""
+
 from uuid import UUID
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from errors import raise_error
-from src.jobs.schemas import JobCreate
+from src.jobs.schemas import (
+    JobCreate,
+    JobsUpdate
+)
 from src.jobs.enums import JobStatus
 from src.jobs.models import Job
 
@@ -11,11 +18,20 @@ from .job_categories import JobCategoryService
 
 
 class JobsService:
+	"""
+		Jobs service class to
+		interact with the `jobs` model
+	"""
 	@staticmethod
 	async def get_by_id(
 			job_id: UUID,
 			db: Session
 	) -> Job|None:
+		"""
+			Finds a job by id
+
+			Returns None if the job is not found
+		"""
 		try:
 			return db.query(Job).filter(Job.id_ == job_id, Job.status.not_in([JobStatus.DELETED])).first()
 		except Exception as exc:
@@ -33,6 +49,11 @@ class JobsService:
 
 	@staticmethod
 	async def find(job_id: UUID, db: Session) -> Job:
+		"""
+			Finds a job by it's id
+
+			Returns an error if the job is not found
+		"""
 		try:
 			job = await JobsService.get_by_id(job_id,db)
 			if not job:
@@ -46,8 +67,37 @@ class JobsService:
 			raise_error(exc)
 	
 
-	async def update(job_id):
-		pass
+	async def update(
+			job_id: UUID,
+			payload: JobsUpdate,
+			db: Session
+	):
+		"""
+			Updates the specified entities of the job model
+
+			Raises an error if:
+			 - the job is not found
+			 - the job category ids are specified and they are not found
+		"""
+		try:
+			job = await JobsService.find(job_id, db)
+
+			if payload.job_category_ids:
+				job_categories = await JobCategoryService\
+				.bulk_find(payload.job_category_ids,db)
+
+				job.job_categories = job_categories
+
+			for item, value in payload.model_dump().items():
+				if value and item != "job_category_ids":
+					setattr(job, item, value)
+
+			db.commit()
+			db.refresh(job)
+
+			return job
+		except Exception as exc:
+			raise_error(exc)
 
 
 	
@@ -57,13 +107,7 @@ class JobsService:
 				.bulk_find(payload.job_category_ids,db)
 			
 			job = Job(
-				title=payload.title,
-				description=payload.description,
-				minimum_salary=payload.minimum_salary,
-				maximum_salary=payload.maximum_salary,
-				company_name=payload.company_name,
-				job_type=payload.job_type,
-				salary_period=payload.salary_period,
+				**payload.model_dump()
 			)
 
 			db.add(job)
