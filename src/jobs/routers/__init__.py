1874c597269e5b39af9afb44cd68eb5f2df6b386
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-02-12 21:54:58 +0100

    add education and work experience router

--- src/jobs/routers/__init__.py
+++ src/jobs/routers/__init__.py
@@ -1,2 +1,4 @@
+from .education import education_router
 from .jobs import jobs_router
 from .job_categories import jobs_router as job_categories_router
+from .work_experience import work_experience_router
