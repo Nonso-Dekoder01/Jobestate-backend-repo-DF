491c1e0adc087d54d050852701145d8574a51a22
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-02-12 21:55:36 +0100

    import new schemas

--- src/jobs/schemas/__init__.py
+++ src/jobs/schemas/__init__.py
@@ -1,5 +1,15 @@
+from .education import (
+    AddEducation, 
+    DisplayEducation
+)
+
 from .jobs import (
     JobCreate, 
     JobCategoryCreate,
     JobsUpdate
+)
+
+from .work_experience import (
+    AddWorkExperience,
+    DisplayWorkExperience
 )
