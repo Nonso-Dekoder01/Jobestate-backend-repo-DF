2a0368cb8b5a637962376184cd77e9a073f919e3
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-02-12 21:53:55 +0100

    import controller classes

--- src/jobs/controllers/__init__.py
+++ src/jobs/controllers/__init__.py
@@ -1,2 +1,4 @@
+from .education import EducationController
 from .jobs import JobsController
 from .job_categories import JobCategoriesController
+from .work_experience import WorkExperienceController
